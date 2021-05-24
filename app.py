5e89a19a38a671a1c29285b9bb9dbbad8065acd0
lzc <415225449@qq.com>
2021-05-24 15:50:16 +0800

    实现：读取movie.db中的数据，并用柱状图和列表展示信息

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 
@@ -6,7 +7,27 @@ app = Flask(__name__)
 # 返回给用用户选然后的文件
 @app.route("/")
 def movie():
-    return render_template("movie.html")
+    datalist = []
+    score = []  # 评分
+    num = []  # 每个评分所统计出的电影数量
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+    print(datalist)
+    print("cname", datalist[0][0])
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(str(item[0]))
+        num.append(item[1])
+
+    cur.close()
+    con.close()
+
+    return render_template("movie.html", movies=datalist, score=score, num=num)
 
 
 if __name__ == '__main__':
